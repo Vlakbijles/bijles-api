db6295af6aacc59c662153cf887d63f3d498a82d
Mark Croes <smarkelton@gmail.com>
2015-06-16 13:26:32 +0200

    goverts kutunderscores

--- common/fbapi.py
+++ common/fbapi.py
@@ -3,7 +3,7 @@ from datetime import date
 from datetime import datetime
 
 
-def calculateAge(birthday):
+def calculate_age(birthday):
     today = date.today()
     bday_object = datetime.strptime(birthday, '%b %d %Y')
     print bday_object
@@ -14,8 +14,7 @@ def calculateAge(birthday):
 # app_id = '1597503327174282'
 # app_secret = '7edf79c0afb8a9858709a176a06a2454'
 
-def getUserData(access_token):
-    # graph = GraphAPI(access_token='CAAWs67pXZCooBAEjXL1X2L1xbUZBJ8myrrUKEzrUk5JfrGzTH7FG42ZA4zhVWv36ORhoTx2huFPz1o5g03cOrBkt9qXKuzYq2Tq7LBrZBxVsEu50UAb1OZBF3Ncq4IV33cEGyb9NFaKVOsB1iCdM9WVK3GHPyoqp0sWR2J7krtZAZBZAUcZCUECVZCH1rcdZBkAK9oZAQtWmQXUoX1K6ZBOayZCoxI')
+def get_user_data(access_token):
     graph = GraphAPI(access_token)
     profile = graph.get_object('me')
     userdata = dict()
@@ -23,4 +22,4 @@ def getUserData(access_token):
     userdata['surname'] = profile['last_name']
     userdata['picture'] = "https://graph.facebook.com/" + profile['id'] + \
                           "/picture?redirect=true&width=200&height=200"
-    print userdata
+    return userdata
