fffc544bcc9eae46e9a9b915cfcde48812355130
Govert Verkes <gverkes@users.noreply.github.com>
2015-06-16 13:30:13 +0200

    getuserdata

--- common/fbapi.py
+++ common/fbapi.py
@@ -7,15 +7,13 @@ def calculateAge(birthday):
     today = date.today()
     bday_object = datetime.strptime(birthday, '%b %d %Y')
     print bday_object
-    return today.year - bday_object.year - ((today.month, today.day) <
-                                           (bday_object.month, bday_object.day))
+    return today.year - bday_object.year - ((today.month, today.day) < (bday_object.month, bday_object.day))
 
 
 # app_id = '1597503327174282'
 # app_secret = '7edf79c0afb8a9858709a176a06a2454'
 
 def getUserData(access_token):
-    # graph = GraphAPI(access_token='CAAWs67pXZCooBAEjXL1X2L1xbUZBJ8myrrUKEzrUk5JfrGzTH7FG42ZA4zhVWv36ORhoTx2huFPz1o5g03cOrBkt9qXKuzYq2Tq7LBrZBxVsEu50UAb1OZBF3Ncq4IV33cEGyb9NFaKVOsB1iCdM9WVK3GHPyoqp0sWR2J7krtZAZBZAUcZCUECVZCH1rcdZBkAK9oZAQtWmQXUoX1K6ZBOayZCoxI')
     graph = GraphAPI(access_token)
     profile = graph.get_object('me')
     userdata = dict()
@@ -23,4 +21,4 @@ def getUserData(access_token):
     userdata['surname'] = profile['last_name']
     userdata['picture'] = "https://graph.facebook.com/" + profile['id'] + \
                           "/picture?redirect=true&width=200&height=200"
-    print userdata
+    return userdata
