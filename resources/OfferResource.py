864d9389866b0ffaf776910aaccae44b941fc540
BDWN <bbx1992@gmail.com>
2015-06-19 22:52:20 +0200

    lijssie ipv dictie

--- resources/OfferResource.py
+++ resources/OfferResource.py
@@ -144,7 +144,7 @@ class OfferResource(Resource):
                 session.commit()
                 return offer, 201
             else:
-                return {}, 200
+                return [], 200
 
         # Create offer when it doesn't already exist
         offer = Offer(user_id=loggedin_data['user_id'],
