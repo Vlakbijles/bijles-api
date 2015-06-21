bd814d73013b99ea2e39b90360ec677a2e236e22
Govert Verkes <gverkes@users.noreply.github.com>
2015-06-21 18:12:59 +0200

    WEET IK VEEL CONFILCT KANKER

--- resources/ReviewResource.py
+++ resources/ReviewResource.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#"!/usr/bin/env python
 """
     ReviewResource.py, for actions on the Review model,
     this file is a module and has no use as stand-alone file
@@ -9,12 +9,13 @@
 
 
 from resources import *  # NOQA
-from models import User
+from models import User, Review, Offer
 
 
 review_fields = {
-    'enorsed': fields.Boolean,
+    'endorsed': fields.Boolean,
     'description': fields.String,
+    'date': fields.DateTime,
     'author.meta.photo_id': fields.String,
     'author.meta.name': fields.String,
     'author.meta.surname': fields.String,
@@ -42,3 +43,43 @@ class ReviewByUserIdResource(Resource):
             reviews += offer.review
 
         return reviews, 200
+
+class ReviewResource(Resource):
+    """
+    Class for handling POST "/review"
+
+    POST is used to create reviews
+
+    """
+
+    @api_validation
+    @authentication(None)
+    @marshal_with(review_fields)
+    def post(self):
+        loggedin_data = loggedin_parser.parse_args(data_parser("loggedin"))
+        review_data = review_parser.parse_args(data_parser("review"))
+
+        # Check if offer exists
+        offer = session.query(Offer).filter(Offer.id == review_data["offer_id"]).first()
+        if not offer:
+            abort(400, message="Offer with id={} doesn't exist".format(review_data["offer_id"]))
+
+        # Check if user has already reviewed this offer
+        user = session.query(Review).filter(Review.offer_id == review_data["offer_id"],
+                                            Review.author_id == loggedin_data["user_id"]).first()
+        if user:
+            return [], 200
+
+        # Create review
+        review = Review(offer_id = review_data["offer_id"],
+                        author_id = loggedin_data["user_id"],
+                        endorsed = review_data["endorsed"])
+
+        if review_data["description"]:
+            # Check if description exceeds max length
+            if len(review_data["description"]) > 500:
+                abort(400, message="Description exceeded max length of 500 chars")
+            review.description = review_data["description"]
+
+        session.add(review)
+        return review, 201
