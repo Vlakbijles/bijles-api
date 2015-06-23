36d3767a28457a134fcd3371ea454f82fa33f3c7
Mark Croes <phineasandperry@hotmail.com>
2015-06-23 15:34:50 +0200

    com

--- email.py
+++ email.py
@@ -1,31 +1,32 @@
 #!/usr/bin/env python
+# Possible way to create a smtp webserver with our server: https://www.digitalocean.com/community/tutorials/how-to-set-up-a-postfix-e-mail-server-with-dovecot
 
 import smtplib
 
 
-def email(sender, contact_offer, contact_msg, recipient):
-	sender = "our@email.com"
-	receivers = recipient
+def sendemail(sender, contact_offer, contact_msg, recipient):
+    sender = "our@email.com"
+    receivers = recipient
 
-	headers = """From: our@email.com
-	To: %s
-	Reply-To: %s
-	Content-type = text/html
-	Subject: Vlakbijles Gebuiker heeft je een bericht gestuurd over %s
-	""" % (recipient, sender, contact_offer)
-	body = contact_msg
+    headers = """From: our@email.com
+    To: %s
+    Reply-To: %s
+    Content-type = text/html
+    Subject: Vlakbijles Gebuiker heeft je een bericht gestuurd over %s
+    """ % (recipient, sender, contact_offer)
+    body = contact_msg
 
-	message = headers + body
+    message = headers + body
 
-	try:
-		server = smtplib.SMTP('vlakbijlessmtphost')
-		# server = smtplib.SMTP(smtp.gmail.com, 587)
-		# gmail = 'vlakbijles@gmail.com'
-		# gmailpassword = 'jsonderulo'
-		# smtpObj.login(gmail, gmailpassword)
-		server.starttls()
-		server.login(onzeemail, password)
-		server.sendmail(sender, receivers, message)
-		print "Succesfully sent email"
-	except
-		print "Error: unable to send email"
+    try:
+        server = smtplib.SMTP('vlakbijlessmtphost')
+        # server = smtplib.SMTP(smtp.gmail.com, 587)
+        # gmail = 'vlakbijles@gmail.com'
+        # gmailpassword = 'jsonderulo'
+        # server.login(gmail, gmailpassword)
+        server.starttls()
+        server.login(onzeemail, password)
+        server.sendmail(sender, receivers, message)
+        print "Succesfully sent email"
+    except:
+        print "Error: unable to send email"
