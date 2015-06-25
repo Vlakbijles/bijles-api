287da9a168d0b251a94221d8a33d512f7e39b94a
Govert Verkes <gverkes@users.noreply.github.com>
2015-06-25 18:40:32 +0200

    yolo

--- common/offer_contact.py
+++ common/offer_contact.py
@@ -1,5 +1,8 @@
 import smtplib
 
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 SERVER = "localhost"
 
 FROM = "noreply@vlakbijles.nl"
@@ -9,19 +12,42 @@ TEXT = "This message was sent with Python's smtplib."
 
 
 def offer_contact(subject, text, recipient, sender):
-    # Message template, insert actual data
-    message = """From: %s
-    To: %s
-    Reply-To: %s
-    Content-type: text/html
-    Subject: %s
+    # Create message container - the correct MIME type is multipart/alternative.
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = FROM
+    msg['Reply-To'] = sender
+    msg['To'] = recipient
 
-    %s
-    """ % (FROM, recipient, sender, subject, text)
+    text = "%s" % (text)
+    html = """\
+    <html>
+      <head></head>
+      <body>
+        <p>
+            %s
+        </p>
+      </body>
+    </html>
+    """ % (text)
+
+    # Record the MIME types of both parts - text/plain and text/html.
+    part1 = MIMEText(text, 'plain')
+    part2 = MIMEText(html, 'html')
+
+    # Attach parts into message container.
+    # According to RFC 2046, the last part of a multipart message, in this case
+    # the HTML message, is best and preferred.
+    msg.attach(part1)
+    msg.attach(part2)
 
     try:
-        server = smtplib.SMTP(SERVER)
-        server.sendmail(FROM, recipient, message)
+        # Send the message via local SMTP server.
+        s = smtplib.SMTP('localhost')
+        # sendmail function takes 3 arguments: sender's address, recipient's address
+        # and message to send - here it is sent as one string.
+        s.sendmail(FROM, recipient, msg.as_string())
+        s.quit()
         print("Successfully sent email")
     except:
         print("Error: unable to send email")
